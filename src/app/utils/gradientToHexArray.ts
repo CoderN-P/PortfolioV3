0d2a5d263b3a1f527c37f227cd983a4aaa64b3dc
CoderN-P <neel.parpia@gmail.com>
2025-07-30 15:29:27 -0700

    Fix build issues

--- src/app/utils/gradientToHexArray.ts
+++ src/app/utils/gradientToHexArray.ts
@@ -3,7 +3,6 @@ import {getColorHex} from "@/app/utils/getColorHex";
 export function gradientToHexArray(gradient: string) {
     const parts = gradient.split(" ");
     return parts.map((p) => {
-        const [_, colorShade] = p.split("-");
         return getColorHex(p.replace(/^(from|via|to)-/, ""));
     });
 }
