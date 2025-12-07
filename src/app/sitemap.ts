35ec2538367e6962d84118c8e96710abd3cc4e73
CoderN-P <neel.parpia@gmail.com>
2025-12-07 15:58:48 -0800

    Update next and react versions

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -28,11 +28,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/ideas`,
-      lastModified: currentDate,
-      priority: 0.7,
-    },
-    {
       url: `${baseUrl}/gallery`,
       lastModified: currentDate,
       priority: 0.7,
